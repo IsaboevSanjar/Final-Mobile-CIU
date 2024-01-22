a5799466c6c94dc26b68a2e09bdbd7d35dc58a25
SanjarIsaboev <sanjarisaboevdev@gmail.com>
2024-01-22 16:02:16 +0500

    Full

--- app/src/main/java/sanjar/uz/final2024january/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/sanjar/uz/final2024january/ui/dashboard/DashboardFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import sanjar.uz.final2024january.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -31,13 +29,7 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val dashboardViewModel =
-            ViewModelProvider(this)[DashboardViewModel::class.java]
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+       
     }
 
     override fun onDestroyView() {
